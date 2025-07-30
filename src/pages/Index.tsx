008cd4b1896a17dcbb7aafe224b4f14852616a53
Ankush Singh <ankushraj2024@gmail.com>
2025-07-31 00:38:51 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -44,7 +44,7 @@ const Index = () => {
       {/* Background Spline 3D iframe */}
       <div className="fixed inset-0 z-0">
         <iframe 
-          src='https://my.spline.design/orb-ufOJhVbQaq8FQkACMyY7Dxf1/' 
+          src='https://my.spline.design/dotwaves-FR8HGbfFmjLusADTS6cmnWBP/' 
           frameBorder='0' 
           width='100%' 
           height='100%'
