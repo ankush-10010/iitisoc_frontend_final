96f050ab3a0fa49daaa21a4dfa53afabe3f0e395
Ankush Singh <ankushraj2024@gmail.com>
2025-08-01 00:16:07 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -73,6 +73,8 @@ export default {
 				md: 'calc(var(--radius) - 2px)',
 				sm: 'calc(var(--radius) - 4px)'
 			},
+			fontFamily: {
+				'pixel':['Press Start 2P', 'cursive']
 			keyframes: {
 				'accordion-down': {
 					from: {
